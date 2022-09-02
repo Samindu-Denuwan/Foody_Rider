ae0ab661fd8294cde9b9dff42a59d4849fa2efd1
Samindu-Denuwan <86373715+Samindu-Denuwan@users.noreply.github.com>
2022-09-03 02:02:22 +0530

    rider getSupport actionbar names

--- app/src/main/java/com/jiat/rider/ui/OrdersFragment.java
+++ app/src/main/java/com/jiat/rider/ui/OrdersFragment.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -51,6 +52,7 @@ public class OrdersFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_orders, container, false);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Orders");
         shopUID = "d6RHVgGQoNZMkciEMVl16lSSsIw2";
         FilterTv = view.findViewById(R.id.filterOrderTv);
         BtnFilter = view.findViewById(R.id.filterBtn);
